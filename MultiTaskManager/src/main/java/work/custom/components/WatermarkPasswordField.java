3f02799c08340c3bcb704b5b3a2ec07899cf693d
Nazarii Ihnat <ignatnazar@gmail.com>
2020-09-22 20:28:18 +0200

    Added watermark password field

--- MultiTaskManager/src/main/java/work/custom/components/WatermarkPasswordField.java
+++ MultiTaskManager/src/main/java/work/custom/components/WatermarkPasswordField.java
@@ -1,4 +1,26 @@
 package work.custom.components;
 
-public class WatermarkPasswordField {
+import utils.Settings;
+
+import javax.swing.*;
+
+public class WatermarkPasswordField extends JPasswordField {
+
+    private final String watermarkText;
+
+    public WatermarkPasswordField(String watermarkText) {
+        this.watermarkText = watermarkText;
+        addFocusListener(new PasswordWatermarker(watermarkText));
+    }
+
+    public void setWatermark() {
+        setEchoChar((char) 0);
+        setText(watermarkText);
+        setForeground(Settings.WATERMARK_TEXT_COLOR);
+
+    }
+
+    public String getWatermarkText() {
+        return watermarkText;
+    }
 }
